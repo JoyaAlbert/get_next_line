dd8db191eb049b5e43f24ef41679f2cc3f8fbf3e
Alberto Joya <albertojoyapincu2@gmail.com>
2023-10-28 09:45:56 +0200

    a

--- get_next.h
+++ get_next.h
@@ -3,21 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   get_next.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajoya-pi <ajoya-pi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joya <joya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:13:51 by ajoya-pi          #+#    #+#             */
-/*   Updated: 2023/10/27 15:00:50 by ajoya-pi         ###   ########.fr       */
+/*   Updated: 2023/10/28 09:43:45 by joya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_H
 # define GET_NEXT_H
-# define BUFFER 1000
+# define BUFFER 4
 
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 #endif
--- get_next_line.c
+++ get_next_line.c
@@ -3,30 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajoya-pi <ajoya-pi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joya <joya@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:14:56 by ajoya-pi          #+#    #+#             */
-/*   Updated: 2023/10/27 15:43:07 by ajoya-pi         ###   ########.fr       */
+/*   Updated: 2023/10/28 09:45:34 by joya             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next.h"
-static char * readfile(fd)
+static char * readfile(int fd)
 {
 	char *aux;
 
 	aux = malloc(BUFFER);
-	if (read(fd, &aux, BUFFER));
+	if (read(fd, &aux, BUFFER) == -1);
 		return (NULL);
 	return(aux);
 }
+
+char *next_line(char *aux)
+{
+	int i;
+	char *line;
+
+	i = 0;
+	while(aux[i] != '\n' || aux[i] != '\0')
+		i++;
+	line = malloc(i * sizeof(char));
+	if (line == NULL)
+		return (NULL);
+		i = 0;
+	while(aux[i] != '\n' || aux[i] != '\0')
+	{
+		line[i] = aux[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
 	static char *aux;
 	char *line;
 
 	aux = NULL;
-	if (aux[BUFFER] != NULL)
+	if (aux[BUFFER] != '\0')
 	{
 		aux = readfile(fd);
 		if (aux == NULL)
@@ -34,6 +56,14 @@ char	*get_next_line(int fd)
 	}
 
 	line = next_line(aux);
-
+	free(aux);
+	printf("%s", line);
+	return (line);
 }
 
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	get_next_line(fd);
+	return 0;
+}
